e07c43b37e9c607fa62254fc6d506963c35ff850
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-20 18:03:22 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,24 +1,73 @@
 <?php
 
-$path = array();
+function getClassPath($URI)
+{
+    $classPath = array();
 
-foreach (explode("/",
-        str_replace("\\", "/", $_SERVER['REQUEST_URI'])
-) as $element) {
+    foreach (explode("/",
+            str_replace("\\", "/", $URI)
+    ) as $element) {
+
+        $element = trim($element);
+
+        if(empty($element))
+            continue;
+
+        if($element=='.')
+            continue;
+
+        if($element=='..')
+            array_pop ($path);
+
+        $classPath[] = $element;
+    }    
     
-    $element = trim($element);
-    
-    if(empty($element))
-        continue;
-    
-    if($element=='.')
-        continue;
-    
-    if($element=='..')
-        array_pop ($path);
-    
-    $path[] = $element;
+    return $classPath;
 }
 
+function getURLs($classPath)
+{
+    $json = __DIR__ . '/../data/' . implode('.', $classPath) . '.json';
+    
+    if(file_exists($json))
+        return array();
+        
+    $json_data = file_get_contents($json);
+    
+    if($json_data===FALSE)
+        return array();
+    
+    $json_object = json_decode($json_data, true);
+    
+    if($json_object===NULL)
+        return array();   
+    
+    if(!is_array($json_object))
+        return array();   
+    
+    return $json_object;   
+}
+
+$context  = stream_context_create(array('http' =>array('method'=>'HEAD')));
+$classPath = getClassPath($_SERVER['REQUEST_URI']);
+
+
+$className = array_pop($classPath);
+$middlePath = [];
+
+while (count($classPath) > 0) {
+
+    foreach(getURLs($classPath) as $repository)
+    {
+        $repositoryFile = $repository.'/'.implode('/', array_reverse($middlePath)).'/'.$className.'.php';
+        
+        $fd = fopen($repositoryFile, 'rb', false, $context);
+        var_dump(stream_get_meta_data($fd));
+        fclose($fd);
+        die($repositoryFile);
+    }
+
+    $middlePath[] = array_pop($classPath);
+}
 
 print_r($path);
