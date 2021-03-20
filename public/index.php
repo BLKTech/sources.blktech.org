e7d1d14eccea98418a2e5d64be5b5d89dc1ee193
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-20 16:09:38 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo $_SERVER['REQUEST_URI'];
+$path = array();
+
+foreach (explode("/",
+        str_replace("\\", "/", $_SERVER['REQUEST_URI'])
+) as $element) {
+    
+    $element = trim($element);
+    
+    if(empty($element))
+        continue;
+    
+    if($element=='.')
+        continue;
+    
+    if($element=='..')
+        array_pop ($path);
+    
+    $path[] = $element;
+}
+
+
+print_r($path);
